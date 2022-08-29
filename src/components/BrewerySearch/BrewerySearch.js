62df7faaa5619f77ad22fa871b08b699ff797bdf
Radivis <radivis@radivis.com>
2022-08-29 13:49:21 +0200

    Implemented combined API queries.

--- src/components/BrewerySearch/BrewerySearch.js
+++ src/components/BrewerySearch/BrewerySearch.js
@@ -5,16 +5,33 @@ import BreweryResults from '../BreweryResults/BreweryResults';
 
 import makeApiCall from '../../helpers/makeApiCall';
 
+
+
 const BrewerySearch = () => {
+
+    const validTypes = [
+        'micro',
+        'nano',
+        'regional',
+        'brewpub',
+        'large',
+        'planning',
+        'bar',
+        'contract',
+        'proprietor',
+        'closed'
+    ]
 
     const dispatch = useDispatch()
 
     const [city, setCity] = useState('')
+    const [type, setType] = useState('')
 
     /* map each state variable name to the corresponding setter
     , so that I don't need to use conditional statements to get the right setter */
     const setterMap = new Map()
     setterMap.set('city', setCity)
+    setterMap.set('type', setType)
 
     const handleChange = ev => {
         setterMap.get(ev.target.name)(ev.target.value)
@@ -23,11 +40,28 @@ const BrewerySearch = () => {
     const handleSubmit = ev => {
         ev.preventDefault()
 
-        makeApiCall({
-            path: 'by_city',
-            dispatch,
-            params: { city }
-        })
+        if (type) {
+            if (city) {
+                makeApiCall({
+                    path: 'combined',
+                    dispatch,
+                    params: { city, type }
+                })
+            } else {
+                makeApiCall({
+                    path: 'by_type',
+                    dispatch,
+                    params: { type }
+                })
+            }
+        } else {
+            makeApiCall({
+                path: 'by_city',
+                dispatch,
+                params: { city }
+            })
+        }
+
     }
 
     return <div>
@@ -36,6 +70,13 @@ const BrewerySearch = () => {
             <div>
                 <label htmlFor="city">City</label>
                 <input type="text" name="city" placeholder="New York City" value={city} onChange={handleChange}></input>
+            </div>
+            <div>
+                <label htmlFor="type">Type</label>
+                <select name="type" value={type} onChange={handleChange}>
+                    <option value="">-Select a type-</option>
+                    {validTypes.map(validType => <option value={validType}>{validType}</option>)}
+                </select>
             </div>
             <button type="submit">Search Breweries</button>
         </form>
--- src/helpers/makeApiCall.js
+++ src/helpers/makeApiCall.js
@@ -5,34 +5,48 @@
 const makeApiCall = ({
     path,
     dispatch,
-    params} = {}) => {
+    params } = {}) => {
     const urlBase = 'https://api.openbrewerydb.org/breweries'
-    
+
     let url = urlBase
 
     switch (path) {
-        case 'by_city':
+        case 'by_city': {
             const underscoreEncodedCity = params.city.toLowerCase().replaceAll(' ', '_')
 
             console.log(underscoreEncodedCity);
 
             url += `?by_city=${underscoreEncodedCity}`
-            break;
-    
+        }
+        break;
+
+        case 'by_type': {
+            url += `?by_type=${params.type}`
+        }
+        break;
+
+        case 'combined': {
+            const underscoreEncodedCity = params.city.toLowerCase().replaceAll(' ', '_')
+
+            console.log(underscoreEncodedCity);
+
+            url += `?by_city=${underscoreEncodedCity}&by_type=${params.type}`
+        }
+
         default:
             break;
     }
 
     fetch(url)
-    .then(res => res.json())
-    .then(data => {
-        // DEBUG
+        .then(res => res.json())
+        .then(data => {
+            // DEBUG
 
-        console.log("data from API call");
-        console.log(data);
+            console.log("data from API call");
+            console.log(data);
 
-        dispatch({type: 'UPDATE_API_RESULTS', data})
-    })
+            dispatch({ type: 'UPDATE_API_RESULTS', data })
+        })
 }
 
 export default makeApiCall
